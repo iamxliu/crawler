13c3e5faf6a462c5c0bea0bdb19b1a90f9caccbc
LiuXing <94578246+iamxliu@users.noreply.github.com>
2022-05-05 18:03:40 +0800

    设计并实现基本的算法 (#2)
    这个提交中完成了基本的方法，并进行了重构，
    要尽可能得使代码通俗易懂。

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -6,23 +6,97 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
+
     public static void main(String[] args) {
+
+
+        //待处理的链接池
+        List<String> linkpool = new ArrayList<>();
+        //已经处理的链接池
+        Set<String> processedLinks = new HashSet<>();
+        linkpool.add("http://sina.cn");
+
+
+        while (true) {
+            if (linkpool.isEmpty()) {
+                break;
+            }
+            String link = linkpool.remove(linkpool.size() - 1);
+            if (processedLinks.contains(link)) {
+                continue;
+            }
+            //不感兴趣 不处理
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkpool::add);
+
+                storeIntoDatabaseIfItIsNewsPage(doc);
+            } else {
+                continue;
+            }
+            processedLinks.add(link);
+        }
+
+
+    }
+
+    private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet("http://sina.cn");
+            if (link.startsWith("//")) {
+                link = "https" + link;
+                System.out.println(link);
+            }
+            HttpGet httpGet = new HttpGet(link);
+            httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36");
+            System.out.println(link);
             try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-//                System.out.println(response1.getCode() + " " + response1.getReasonPhrase());
                 System.out.println(response1.getStatusLine());
                 HttpEntity entity1 = response1.getEntity();
-                EntityUtils.consume(entity1);
+                String html = EntityUtils.toString(entity1);
+                return Jsoup.parse(html);
             }
-
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link);
+
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "http://sina.cn".equals(link);
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
+    }
+
 }
