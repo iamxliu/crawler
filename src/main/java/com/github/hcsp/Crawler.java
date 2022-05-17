93b9f574dcac8d325ceb8d550efa0d8f9318fdc7
lx <iamxliu@163.com>
2022-05-17 14:56:13 +0800

    重构成不同模块

---
+++ src/main/java/com/github/hcsp/Crawler.java
@@ -0,0 +1,112 @@
+package com.github.hcsp;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+
+public class Crawler {
+
+    private CrawlerDao dao = new JdbcCrawlerDao();
+
+
+    public void run() throws SQLException {
+
+        String link = null;
+        //从数据库拿出一个链接并删掉，准备处理, 如果有链接，就进行循环
+        while ((link = dao.getNextLinkThenDelete()) != null) {
+
+            //询问数据库，当前链接是否已经处理过
+            if (dao.isLinkProcessed(link)) {
+                continue;
+            }
+            //不感兴趣 不处理
+            if (isInterestingLink(link)) {
+                System.out.println(link);
+                Document doc = httpGetAndParseHtml(link);
+                parseUrlsFromPageAndStoreIntoDatabase(doc);
+                storeIntoDatabaseIfItIsNewsPage(doc, link);
+                dao.updateDatabase("INSERT INTO LINKS_ALREADY_PROCESSED (link) values (?)", link);
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        new Crawler().run();
+    }
+
+
+    private void parseUrlsFromPageAndStoreIntoDatabase(Document doc) throws SQLException {
+        for (Element aTag : doc.select("a")) {
+            String href = aTag.attr("href");
+            if (href.startsWith("//")) {
+                href = "https" + href;
+                System.out.println(href);
+            }
+            if (!href.toLowerCase().startsWith("javascript")) {
+                dao.updateDatabase("insert into LINKS_TO_BE_PROCESSED (link) values (?)", href);
+            }
+        }
+    }
+
+
+    private void storeIntoDatabaseIfItIsNewsPage(Document doc, String link) throws SQLException {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                String content = articleTag.select("p").stream().map(Element::text).collect(Collectors.joining("\n"));
+                dao.insertNewsIntoDatabase(link, title, content);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+
+            HttpGet httpGet = new HttpGet(link);
+            httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36");
+            System.out.println(link);
+            try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+                HttpEntity entity1 = response1.getEntity();
+                String html = EntityUtils.toString(entity1);
+                return Jsoup.parse(html);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link);
+
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isIndexPage(String link) {
+
+        return "https://sina.cn".equals(link);
+
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
+    }
+
+}
